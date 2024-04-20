a3577599bd44991f0b4eb5296def94bafbf70ea2
EmersonJr <contato.juniormorales@gmail.com>
2024-04-20 11:36:44 -0300

    att

--- meu-app/src/App.js
+++ meu-app/src/App.js
@@ -1,41 +1,43 @@
 import React from 'react';
 import './App.css';
 
-function App({char, searchTerm, searchTermChanged}) {
+function App({ char, searchTerm, searchTermChanged }) {
   return (
-    <section>
+    <section className="scrollable-content">
       <div id="header">
-        <h1>DC Superheroes</h1>
-        <h3>A List of Major DC Superheroes</h3>
+        <h1>DC Superherois</h1>
+        <h3>Uma lista dos principais super-heróis da DC</h3>
       </div>
       <form>
         <div className="search">
           <input
             type="text"
             name="search"
-            placeholder="Search"
+            placeholder="Procurar heroi"
             value={searchTerm}
-            onChange={e => searchTermChanged(e.target.value)}
+            onChange={(e) => searchTermChanged(e.target.value)}
           />
         </div>
       </form>
 
       <table>
         <thead>
-          <tr style={{textAlign: 'center'}}>
-            <th>Name</th>
+          <tr style={{ textAlign: 'center' }}>
+            <th>Nome</th>
             <th>Alter Ego</th>
-            <th>First Appearance in</th>
-            <th>Image</th>
+            <th>Primeira aparição em</th>
+            <th>Imagem</th>
           </tr>
         </thead>
         <tbody>
-          {char.map(curChar => (
+          {char.map((curChar) => (
             <tr key={curChar.name}>
               <td>{curChar.name}</td>
               <td>{curChar.alter_ego}</td>
               <td>{curChar.first_appearance}</td>
-              <img src={require(`./images/View/${curChar.name}.png`)} />
+              <td>
+                <img src={require(`./images/View/${curChar.name}.png`)} />
+              </td>
             </tr>
           ))}
         </tbody>
